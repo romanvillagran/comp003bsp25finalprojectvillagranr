b3238176394741865981cf9a0c365f9b3872752b
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:59:15 -0700

    sdded content to exercise

--- comp003bsp25finalprojectvillagranr/Models/Exercise.cs
+++ comp003bsp25finalprojectvillagranr/Models/Exercise.cs
@@ -2,5 +2,9 @@
 {
     public class Exercise
     {
+        public int Id { get; set; }
+        public string Name { get; set; }  
+        public string MuscleGroup { get; set; }  
+        public ICollection<Workout>? Workouts { get; set; }
     }
 }
