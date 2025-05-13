2a1f5dce48a1b4480b2adb7507dc23e2e2144339
cbig3 <romanvillagran386@gmail.com>
2025-05-12 22:55:00 -0700

    added workout file with content and exercise

---
+++ comp003bsp25finalprojectvillagranr/Models/Workout.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace comp003bsp25finalprojectvillagranr.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Workout name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime Date { get; set; }
+
+        [Range(1, 500, ErrorMessage = "Calories must be 1-500")]
+        public int CaloriesBurned { get; set; }
+
+        public int ExerciseId { get; set; }
+        public Exercise? Exercise { get; set; }
+    }
+}
